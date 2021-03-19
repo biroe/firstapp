e6b6b06e54cf001be90f3a2b1bae443538feea2c
Ryo Fuj <ryo176999@gmail.com>
2021-03-18 18:13:15 -0700

    first app practice
    car - compare, player/computer, scores

---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by ryo fujimura on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
