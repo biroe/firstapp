cca8fd5eed1d289ede2282e6d542bf47fdfb1bb9
Ryo Fuj <ryo176999@gmail.com>
2021-12-11 22:57:08 -0800

    Group, Fiie neumorphism

---
+++ Shared/Neumorphism/NeumorphismButton.swift
@@ -0,0 +1,20 @@
+//
+//  NeumorphismButton.swift
+//  firstapp (iOS)
+//
+//  Created by ryo fuj on 12/11/21.
+//
+
+import SwiftUI
+
+struct NeumorphismButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NeumorphismButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NeumorphismButton()
+    }
+}
