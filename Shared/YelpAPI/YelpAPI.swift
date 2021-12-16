5cd8decbeadc8168803e04ebebfff5471b4af108
Ryo Fuj <ryo176999@gmail.com>
2021-12-16 09:59:21 -0800

    YelpAPI file

--- Shared/YelpAPI/YelpAPI.swift
+++ Shared/YelpAPI/YelpAPI.swift
@@ -13,7 +13,7 @@ let apikey = ""
 
 struct YelpAPI {
     // search term, user location, category
-    var search: (String, CLLocation, String?) -> AnyPublisher<SearchResult, Never>
+    var search: (String, CLLocation, String?) -> AnyPublisher<[Business], Never>
 }
 
 extension YelpAPI {
@@ -33,11 +33,60 @@ extension YelpAPI {
         return URLSession.shared.dataTaskPublisher(for: request)
             .map(\.data)
             .decode(type: SearchResult.self, decoder: JSONDecoder())
-            .replaceError(with: .init())
+            .map(\.businesses)
+            .replaceError(with: [])
+            .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
 }
 
 struct SearchResult: Codable {
-    
+    let businesses: [Business]
+}
+
+// MARK: - Business
+struct Business: Codable {
+    let rating: Int?
+    let price, phone, id, alias: String?
+    let isClosed: Bool?
+    let categories: [Category]?
+    let reviewCount: Int?
+    let name: String?
+    let url: String?
+    let coordinates: Coordinates?
+    let imageURL: String?
+    let location: Location?
+    let distance: Double?
+    let transactions: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case rating, price, phone, id, alias
+        case isClosed = "is_closed"
+        case categories
+        case reviewCount = "review_count"
+        case name, url, coordinates
+        case imageURL = "image_url"
+        case location, distance, transactions
+    }
+}
+
+// MARK: - Category
+struct Category: Codable {
+    let alias, title: String?
+}
+
+// MARK: - Coordinates
+struct Coordinates: Codable {
+    let latitude, longitude: Double?
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let city, country, address2, address3: String?
+    let state, address1, zipCode: String?
+
+    enum CodingKeys: String, CodingKey {
+        case city, country, address2, address3, state, address1
+        case zipCode = "zip_code"
+    }
 }
