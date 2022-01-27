d29662d7c65d7b5fc3c21ab6cfa4f0a664dc39e7
Ryo Fuj <ryo176999@gmail.com>
2022-01-26 23:51:48 -0800

    init

---
+++ Shared/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  Shared
+//
+//  Created by ryo fuj on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
